2789109f3d44d3f90ab1d0b2068138d8f7932127
Schmanub <asmanu21@gmail.com>
2024-02-21 19:40:47 -0500

    Added task marking as completed and task deletion features

--- main.py
+++ main.py
@@ -3,14 +3,28 @@ todo_list = []
 def add_task(task):
     todo_list.append(str(task))
 def view_tasks():
-    for task in todo_list:
-        print("TO DO LIST: ")
-        print(task + "\n")
+    print("TO DO LIST: ")
+    for i, task in enumerate(todo_list):
+        print(str(i+1) + ") " + task)
+
+def mark_task(index_num):
+    if "(Completed)" not in todo_list[index_num]:
+        todo_list[index_num] = todo_list[index_num] + " (Completed)"
+
+def delete_task(index_num):
+    if (index_num < len(todo_list)) and (index_num > -1):
+        del todo_list[index_num]
 
 while True:
-    user_input = input("What do you want to do, type 'add' to add a task, 'view' to view all tasks")
+    user_input = input("What do you want to do, type 'add' to add a task, 'view' to view all tasks, 'mark' to mark one as completed: ")
     if str(user_input) == "view":
         view_tasks()
     elif(str(user_input) == "add"):
-        input_task = input("Type the task that you want to add to the list")
+        input_task = input("Type the task that you want to add to the list: ")
         add_task(input_task)
+    elif(str(user_input) == "mark"):
+        user_input = input("Type the number of the task that you want to mark as completed: ")
+        mark_task(int(user_input)-1)
+    elif(str(user_input) == "delete"):
+        user_input = input("Type the number of the task that you want to delete: ")
+        delete_task(int(user_input)-1)
